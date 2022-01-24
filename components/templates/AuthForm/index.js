4cbb4c08d54aea0c725280f2fc2dcfa7306423e6
FilipeABBarroso <filipebarroso2812@gmail.com>
2022-01-24 19:57:37 +0000

    Update auth page

--- components/templates/AuthForm/index.js
+++ components/templates/AuthForm/index.js
@@ -1,36 +1,40 @@
 import React from "react";
 import styles from "./style.module.css";
-import classNames from "classnames";
-import TextField from "../../TextField";
+import { useState } from "react";
 import { useRouter } from "next/router";
+import { QRCode } from "react-qr-svg";
 
 export default function Auth(){
-    const router = useRouter();
+  const [code, setCode] = useState('');
+  const router = useRouter();
 
-    const handleForm = () => {
-        router.push('/');
-    };
+  const handleSubmit = () => {
 
-    return (
-        <>
-            <div className={styles.outerWrapper}>
-                <div className={styles.wrapper}>
-                    <div className={classNames(['box', styles.container])}>
-                        <h1 className={styles.h1}>Authenticate Your Account</h1>
-                        <p className={styles.p}>Protecting your tickets is our top priority. Please confirm your
-                            account by entering the authorization code.</p>
-                        <form className={styles.form} onSubmit={handleForm}>
-                            <div className={styles.inputs}>
-                                <label>Code</label>
-                                <input input='text' />
-                            </div>
-                            <div className={styles.inputs}>
-                                <button>Submit</button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </>
-    );
+  };
+
+
+
+  return (
+    <>
+      <div className={styles.root}>
+        <h1 className={styles.h1}>Scan the QRCode :)</h1>
+        <div className={styles.qrcode}>
+          <QRCode
+            level="Q"
+            className={styles.qrSize}
+            value={JSON.stringify({
+              secret: "JF2SYTLEKRVT4OS3MMYC67K6KRVUUT2MHAZTQKTMJ4XTAUZGONJA",
+              token:
+                "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1dWlkIjoiMGQ3MmY0ZmMtMTk2OS00ZjBmLTk3MDctMzM2OTVhMmYxMjA2IiwiaWF0IjoxNjQyOTgwMjk3fQ.p_5b95Z0q7eZXmoRLdApo-EuCSNpnACnV9obpleCXJU"
+            })}
+          />
+        </div>
+        <h1 className={styles.h1}>Insere já o código bro</h1>
+        <input type='text' value={code} required onChange={(e) => {
+          setCode(e.target.value);
+        }} />
+        <button onClick={handleSubmit}>submit</button>
+      </div>
+    </>
+  );
 }
